4a51e5418a76593ce32ef960df08332fe300d2ba
chaman199 <chamanjangra1995@gmail.com>
2020-08-31 15:59:48 +0530

    some ui changes

---
+++ public/precache-manifest.5f75a9af49fd61f28579b92ee1d9ab68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2131dbc248a4beac6bd81d3b3ea8048",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22fc06ada9bda4b53ad0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1ec9604cd064e0eca31d",
+    "url": "/static/css/main.a5136d75.chunk.css"
+  },
+  {
+    "revision": "22fc06ada9bda4b53ad0",
+    "url": "/static/js/2.3cc878aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3cc878aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec9604cd064e0eca31d",
+    "url": "/static/js/main.53df2d10.chunk.js"
+  },
+  {
+    "revision": "9091f47bb88943cb9e61",
+    "url": "/static/js/runtime-main.89d020cb.js"
+  }
+]);
